b65b6ff8cc641163372319e228cb1a549bb9a020
Dom Le <lequydung101@yahoo.com>
2020-12-06 21:33:36 +1100

    all 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {Fragment, useContext } from 'react';
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar.js'
@@ -7,17 +8,57 @@ import Users from './components/users/Users.js'
 import User from './components/users/User.js'
 import Search from './components/users/Search.js'
 import About from './components/pages/About.js'
-import AppStateContext, {AppContext} from './AppStateContext'
 
 
 const App = () => {
 
-  const appContext = useContext(AppContext)
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({})
+  const [repos, setRepos] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [alert, setAlert] = useState(null)
+ 
+  const searchUsers = async (text) => {
 
-  // const { searchUsers, clearUsers, showAlert, getUser, getUserRepos, loading, users, repos, user } = appContext;
-  
+    setLoading(true)
+      
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    
+    setUsers(res.data.items)
+    setLoading(false)
+    setAlert(null)
+  }
+   
+  //Get a single github user
+  const getUser = async (username) => {
+    setLoading(true)
+      
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    
+    setUser(res.data)
+    setLoading(false)
+  }
+
+  //Get Repositories from a single github user
+  const getUserRepos = async (username) => {
+    setLoading(true)
+      
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    
+    setRepos(res.data)
+    setLoading(false)
+  }
+
+  const clearUsers = () => {
+    setUsers([])
+    setLoading(false)
+  }
+
+  const showAlert = (msg, type) => {
+    setAlert({msg, type})
+  }
+
   return (
-    <AppStateContext>
       <Router>
         <div className='App' >
           <Navbar /> 
@@ -28,12 +69,12 @@ const App = () => {
                   <Fragment>
                     <Alert alert={alert} />
                       <Search
-                        searchUsers={appContext.searchUsers}
-                        clearUsers={appContext.clearUsers}
-                        showClear={appContext.users.length > 0 ? true : false}
-                        setAlert = {appContext.showAlert}
+                        searchUsers={searchUsers}
+                        clearUsers={clearUsers}
+                        showClear={users.length > 0 ? true : false}
+                        setAlert = {showAlert}
                       />
-                      <Users loading={appContext.loading}  users={appContext.users} />
+                      <Users loading={loading}  users={users} />
                   </Fragment>
                 ) 
               }} />
@@ -46,18 +87,17 @@ const App = () => {
                 return (
                   <User
                     {...props}
-                    getUser={appContext.getUser}
-                    getUserRepos={appContext.getUserRepos}
-                    user={appContext.user}
-                    repos={appContext.repos}
-                    loading={appContext.loading} />
+                    getUser={getUser}
+                    getUserRepos={getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading} />
                 )
               }}/>
             </Switch> 
           </div>
         </div>
       </Router>
-    </AppStateContext>
   ) 
   
 }
